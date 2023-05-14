5c90b306122ce2ce341a97df1e1e11f2bedf95b0
Greg Jones <gregj@thesoftwarecottage.com.au>
2023-05-14 22:35:18 +1000

    Part 4

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import {Link} from 'gatsby'
-import {container, navLinkItem, navLinks, navLinkText, heading} from './layout.module.css'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import {container, navLinkItem, navLinks, navLinkText, heading, siteTitle} from './layout.module.css'
 
 export class LayoutProps {
     pageTitle?: string;
@@ -8,8 +8,19 @@ export class LayoutProps {
 }
 
 const Layout = (layout: LayoutProps) => {
+    const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
     return (
         <div className={container}>
+            <header className={siteTitle}>{data.site.siteMetadata.title}</header>
             <nav>
                 <ul className={navLinks}>
                     <li className={navLinkItem}>
@@ -17,6 +28,11 @@ const Layout = (layout: LayoutProps) => {
                     </li>
                     <li className={navLinkItem}>
                         <Link to="/about" className={navLinkText}>About</Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/blog" className={navLinkText}>
+                            Blog
+                        </Link>
                     </li>
                 </ul>
             </nav>
